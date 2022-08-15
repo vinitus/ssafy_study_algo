71c527450f57e68f387e0e8184f68cf8d63d4d61
vinitus <tlsdnrng@gmail.com>
2022-08-15 20:39:31 +0900

    0815

--- vinitus/2493_탑.py
+++ vinitus/2493_탑.py
@@ -5,11 +5,15 @@ input = sys.stdin.readline
 N = int(input())
 
 lst = list(map(int, input().split()))
-lst_idx = [0] * N
-stk = [N-1]
-for i in range(N-2, -1, -1):
+result_lst = [0] * N       
+stk = []
+for i in range(N-1, -1, -1):
     while stk and lst[stk[-1]] <= lst[i]:
-        lst_idx[stk.pop()] = i+1
+        result_lst[stk.pop()] = i+1
     stk.append(i)
 
-print(' '.join(map(str,lst_idx)))
+print(' '.join(map(str,result_lst)))
+
+# ' '.join(list) vs *list
+# 메모리: 많이, 적게
+# 속  도: 빠름, 느림
