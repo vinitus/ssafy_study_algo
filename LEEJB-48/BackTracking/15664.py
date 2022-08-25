690844ef92478087f8e956eb79348b80025a3ae1
ljbsju48 <ljbsju48@gmail.com>
2022-08-25 16:45:19 +0900

    0825 15664

---
+++ LEEJB-48/BackTracking/15664.py
@@ -0,0 +1,22 @@
+import sys
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+dic = {}
+def backtracking(i,ret):
+
+    if len(ret) == M:
+        dic[tuple(ret)] = True
+        return None
+    if i == N:
+        return None
+
+    backtracking(i+1,ret+[nums[i]])
+    backtracking(i+1,ret)
+
+N,M = map(int,input().split())
+nums = sorted(list(map(int,input().split())))
+backtracking(0,[])
+for key in dic.keys():
+    print(*key)
