278ad33c49f9d1eaa212a73aeacf1bc06dc013de
vinitus <tlsdnrng@gmail.com>
2022-09-02 17:53:45 +0900

    ok

--- vinitus/03_week_tree/1068_트리.py
+++ vinitus/03_week_tree/1068_트리.py
@@ -6,10 +6,20 @@ N = int(input())
 lst = list(map(int,input().split()))
 visited = [0] * N
 to_move = int(input())
-lst.sort()
-cnt = N
-q = []
-flag = False
+visited[to_move] = 1
+lst[to_move] = -1
+cnt = N - 1
+q = [to_move]
+
+while q:
+    tmp = q.pop()
+    for i in range(N):
+        if lst[i] == tmp:
+            q.append(i)
+            if not visited[i]:
+                visited[i] = 1
+                lst[i] = -1
+                cnt -= 1
 
 for i in range(N-1,-1,-1):
     if lst[i] < 0:
---
+++ vinitus/03_week_tree/5639_이진검색트리.py
@@ -0,0 +1,53 @@
+import sys
+def input():
+    return sys.stdin.readline().rstrip()
+
+def check(idx):
+    return 0<=idx<N
+
+class Node:
+    def __init__(self,data,up=None,left=None,right=None):
+        self.data = data
+        self.up = up
+        self.left = left
+        self.right = right
+
+def upup(node):
+    return node.up
+
+lst = []
+a = input()
+
+while a != "":
+    lst.append(int(a))
+    a = input()
+
+N = len(lst)
+
+
+tree = {lst[0]:Node(lst[0])}
+for i in range(1,N):
+    if check(i-1) and lst[i-1] > lst[i]:
+        tree[lst[i-1]].left = lst[i]
+        tree[lst[i]] = Node(lst[i])
+        tree[lst[i]].up = lst[i-1]
+    else:
+        idx = i-1
+        while tree[lst[idx]].up is not None:
+            if tree[lst[idx]].right is None and (tree[lst[idx]].left is not None or tree[lst[idx]].up > lst[i]):
+                tree[lst[idx]].right = lst[i]
+                tree[lst[i]] = Node(lst[i],up=lst[idx])
+                break
+            idx -= 1
+        else:
+            tree[lst[0]].right = lst[i]
+            tree[lst[i]] = Node(lst[i], up=lst[0])
+
+def back(node):
+    if node.left:
+        back(tree[node.left])
+    if node.right:
+        back(tree[node.right])
+    print(node.data)
+
+back(tree[lst[0]])
