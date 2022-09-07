ff401f7c222d7dd96824e2cae72dbfdd217a45ca
vinitus <tlsdnrng@gmail.com>
2022-09-07 20:25:42 +0900

    스터디준비

--- vinitus/03_week_tree/3584_가장가까운공통조상_more.py
+++ vinitus/03_week_tree/3584_가장가까운공통조상_more.py
@@ -3,18 +3,25 @@ import sys
 input = sys.stdin.readline
 
 def find_parent(a, b):
+    # 조상 리스트 만들기
     a_parent = [a]
     while tree[a]:
         a = tree[a]
         a_parent.append(a)
 
+    # 조상 중 하나가 b다
     if b in a_parent:
         return b
 
+    # b의 조상을 찾아서 올라감. a랑 겹치면 바로 return
     while tree[b]:
         b = tree[b]
         if b in a_parent:
             return b
+# 저는 B를 만들었는데 이 분은 B를 만들지 않았음.
+# 저는 A, B 둘 다 트리를 타고 끝까지 올라갔는데 이 분은 만들어둔 A 트리를 B를 통해서 타고 올라감
+# if 구문을 통한 최적화도 되어있음
+# 저는 1500ms 나왔고 이분은 92ms
 
 
 T = int(input())
