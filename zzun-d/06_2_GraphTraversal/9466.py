17830bb35238d02ad08e128d0b0c661348969be9
zzun-d <dlwnsgur0803@gmail.com>
2022-10-19 19:11:57 +0900

    algo

--- zzun-d/06_2_GraphTraversal/9466.py
+++ zzun-d/06_2_GraphTraversal/9466.py
@@ -4,12 +4,16 @@ for _ in range(T):
     lst = [0] + list(map(int, input().split()))
     visited = [0]*(N+1)
     cnt = 0
+
     for i in range(1, N+1):
         v_lst = []
+
         while not visited[i]:
             visited[i] = 1
             v_lst.append(i)
             i = lst[i]
+
         if i in v_lst:
             cnt += len(v_lst) - v_lst.index(i)
+            
     print(N - cnt)
